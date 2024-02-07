1a70cc7a2d96bd5c6b86e862bb7057a9bb895ff4
Tianyi Yang <tianyi_yang@ucsb.edu>
2024-02-07 12:35:55 -0800

    Menus Tables done - T. Yang

---
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItems.java
@@ -0,0 +1,21 @@
+package edu.ucsb.cs156.example.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "ucsbdiningcommonsmenuitems")
+public class UCSBDiningCommonsMenuItems {
+  @Id
+  private String code;
+  private String name;
+  private String station;
+}
