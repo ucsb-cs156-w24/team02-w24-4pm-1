2f8ea327b027a7ca276632cb2e0ddcfb63a03e36
Tianyi Yang <tianyi_yang@ucsb.edu>
2024-02-07 13:02:27 -0800

    altered table to use AUTO INCREMENT as primary key - T. Yang

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItems.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItems.java
@@ -1,7 +1,9 @@
 package edu.ucsb.cs156.example.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,7 @@ import lombok.Builder;
 @Entity(name = "ucsbdiningcommonsmenuitems")
 public class UCSBDiningCommonsMenuItems {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private String code;
   private String name;
   private String station;
--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemsRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemsRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UCSBDiningCommonsMenuItemsRepository extends CrudRepository<UCSBDiningCommonsMenuItems, String> {
+public interface UCSBDiningCommonsMenuItemsRepository extends CrudRepository<UCSBDiningCommonsMenuItems, Long> {
  
 }
